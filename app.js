11e57942e635d89c2e3a423cee59b20fc7a69c81
DESKTOP-V77LQF3\LeeJeongmin <ljm245@naver.com>
2018-09-27 18:57:29 +0900

    Grid

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testCSSRouter = require('./routes/testCSS');
 var testCSS2Router = require('./routes/testCSS2');
+var gridRouter = require('./routes/grid');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testCSS',testCSSRouter);
 app.use('/testCSS2',testCSS2Router);
+app.use('./grid',gridRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
